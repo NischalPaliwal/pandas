66239c14decdc5035806832df5695943563279d2
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-06 17:13:30 +0530

    code added

--- pandas6.py
+++ pandas6.py
@@ -12,4 +12,13 @@ pivot = coffee.pivot(index='Day', columns='Coffee Type', values='Units Sold')
 # print(bios.groupby('Year').agg({'Name': 'count'}).reset_index().sort_values('Name'))
 
 coffee['Yesterday Units Sold'] = coffee['Units Sold'].shift(2)
-print(coffee)
+coffee['% Change'] = coffee['Units Sold'] / coffee['Yesterday Units Sold'] * 100
+coffee['Units Sold CumSum'] = coffee['Units Sold'].cumsum()
+# print(coffee.head())
+
+bios['Age Rank'] = bios['Age'].rank(ascending=True, method='dense')
+# print(bios.sort_values('Age Rank', ascending=True).head(7)[['Age', 'Age Rank']])
+
+latte = coffee[coffee['Coffee Type'] == 'Latte'].copy()
+latte['Past 3 days revenue'] = latte['Units Sold'].rolling(3).sum() * 60
+print(latte.head())
