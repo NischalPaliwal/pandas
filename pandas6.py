cd6724955ffbff11ac43c1ae5e02cdaaa2220838
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-06 15:52:24 +0530

    Refactor code by commenting out print statements for clarity

--- pandas6.py
+++ pandas6.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
 bios = pd.read_csv('files/athlete_events.csv')
-print(bios['City'].value_counts())
+# print(bios['City'].value_counts())
+
+coffee = pd.read_csv('files/coffee.csv')
+# print(coffee.groupby(['Coffee Type']).agg({'Units Sold': 'sum', 'Day': 'count'}))
+
+pivot = coffee.pivot(index='Day', columns='Coffee Type', values='Units Sold')
+# print(pivot.head())
+
+# print(bios.groupby('Year').agg({'Name': 'count'}).reset_index().sort_values('Name'))
+
+coffee['Yesterday Units Sold'] = coffee['Units Sold'].shift(2)
+print(coffee)
